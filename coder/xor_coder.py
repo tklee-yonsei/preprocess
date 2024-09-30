8eaba5e66029085f2f47728cab9e7ce7fe32f733
TKLee Yonsei <taekyulee@yonsei.ac.kr>
2024-09-30 13:11:25 +0900

    + Add new coder

--- coder/xor_coder.py
+++ coder/xor_coder.py
@@ -2,8 +2,16 @@ from .interfaces import ChannelCoder
 
 
 class XORCoder(ChannelCoder):
+    def __init__(self, key="1010"):
+        self.key = [int(bit) for bit in key]
+
     def encode(self, data_bits):
-        return data_bits
+        data_bits = [int(bit) for bit in data_bits]
+        return self._xor_operation(data_bits)
 
     def decode(self, coded_bits):
-        return coded_bits
+        coded_bits = [int(bit) for bit in coded_bits]
+        return self._xor_operation(coded_bits)
+
+    def _xor_operation(self, bits):
+        return [(bit ^ self.key[i % len(self.key)]) for i, bit in enumerate(bits)]
